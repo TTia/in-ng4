f4f980ca33df80e17c74fbf4660b001cf893ebc0
TTia <mattia.barrasso@gmail.com>
2017-07-20 19:39:37 +0200

    NG4 Init

---
+++ NG4/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NG4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return new FilePathResult("src/index.html", "text/html");
+        }
+    }
+}
